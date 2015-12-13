4628702015d703efe904d3ee7fc7fa935c77fdf8
bwolf88 <anidorybp@gmail.com>
2015-12-13 13:07:50 +0300

    Empty project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class GameManager: MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameManager gm = new GameObject("GameManager").AddComponent<GameManager>();
+                _instance = gm;
+            }
+            return _instance;
+        }
+    }
+
+
+    void Awake()
+    {
+        if (_instance == null) { _instance = this; }
+        else { Destroy(this.gameObject); }
+    }
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+}
