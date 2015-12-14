db6195d77e22d80bae8f6298dc149adc0b6432f2
bwolf88 <anidorybp@gmail.com>
2015-12-14 20:01:48 +0300

    Pac man base controller

---
+++ Assets/Scripts/Persona/Direction.cs
@@ -0,0 +1,9 @@
+﻿public enum Direction
+{
+    Left = 0,
+    Right = 1,
+    Up = 2,
+    Down = 3,
+
+    None = 4,
+}
---
+++ Assets/Scripts/Persona/PacManController.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PacManController: MonoBehaviour
+{
+    public static float defaultspeed = 5;
+    public float speedMod = 1;
+    public float speedModFright;
+
+    private bool _isDead = false;
+    public bool IsDead { get { return _isDead; } }
+
+    private float _activeSpeedMod;
+
+    private Animator _anim;
+    private Transform _myTrans;
+
+    private Direction _direction = Direction.None;
+    public Vector3 _nextPosition;
+
+    void Awake()
+    {
+        _myTrans = this.transform;
+        _anim = this.GetComponent<Animator>();
+        _activeSpeedMod = speedMod;
+    }
+
+	void Start ()
+    {
+        CheckDirection(Direction.Left, _myTrans.position);
+    }
+
+    void Update ()
+    {
+        if (_direction != Direction.None) MoveToNextPosition();
+
+        if (!IsDead)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                CheckDirection(Direction.Left, _myTrans.position);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                CheckDirection(Direction.Right, _myTrans.position);
+            }
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                CheckDirection(Direction.Up, _myTrans.position);
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                CheckDirection(Direction.Down, _myTrans.position);
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                OnDie();
+            }
+        }
+    }
+
+    private void CheckDirection(Direction dir, Vector3 position)
+    {
+        if (GameManager.Instance.Map == null) return;
+        if (dir == Direction.None) return;
+
+        int ix = Mathf.FloorToInt(position.x);
+        int iy = Mathf.FloorToInt(position.y);
+
+        if (dir == Direction.Left && !GameManager.Instance.Map[ix - 1, iy].IsObstacle)
+        {
+            if (position.y - iy < 0.25f || position.y - iy > 0.75f) return;
+            _nextPosition = new Vector3(ix - 1 + 0.5f, iy + 0.5f);
+            _direction = dir;
+        }
+
+        else if (dir == Direction.Right && !GameManager.Instance.Map[ix + 1, iy].IsObstacle)
+        {
+            if (position.y - iy < 0.25f || position.y - iy > 0.75f) return;
+            _nextPosition = new Vector3(ix + 1 + 0.5f, iy + 0.5f);
+            _direction = dir;
+        }
+
+        else if (dir == Direction.Down && !GameManager.Instance.Map[ix, iy - 1].IsObstacle)
+        {
+            if (position.x - ix < 0.25f || position.x - ix > 0.75f) return;
+            _nextPosition = new Vector3(ix + 0.5f, iy - 1 + 0.5f);
+            _direction = dir;
+        }
+
+        else if (dir == Direction.Up && !GameManager.Instance.Map[ix, iy + 1].IsObstacle)
+        {
+            if (position.x - ix < 0.25f || position.x - ix > 0.75f) return;
+            _nextPosition = new Vector3(ix + 0.5f, iy + 1 + 0.5f);
+            _direction = dir;
+        }
+        _anim.SetInteger("Direction", (int)_direction);
+    }
+
+    private void MoveToNextPosition()
+    {
+        Vector3 position = _myTrans.position;
+        AutoCorrectPosition(ref position, ref _nextPosition);
+
+        _myTrans.position = Vector3.MoveTowards(position, _nextPosition, Time.deltaTime * defaultspeed * _activeSpeedMod);
+
+        int ix = Mathf.FloorToInt(position.x);
+        int iy = Mathf.FloorToInt(position.y);
+
+        if ((position.y - iy > 0.4f || position.y - iy < 0.6f)
+            && (position.x - ix > 0.4f || position.x - ix < 0.6f))
+        {
+            //gameManager.EatFood(px, py);
+            CheckDirection(_direction, _myTrans.position);
+        }
+    }
+
+    private void AutoCorrectPosition(ref Vector3 position, ref Vector3 nextPosition)
+    {
+        int ix = Mathf.FloorToInt(_myTrans.position.x);
+        int iy = Mathf.FloorToInt(_myTrans.position.y);
+
+        if (ix < 0)
+        {
+            position.x = GameManager.Instance.Map.SizeX;
+            nextPosition.x = position.x - 0.5f;
+        }
+        else if (ix > GameManager.Instance.Map.SizeX)
+        {
+            position.x = 0; nextPosition.x = 0.5f;
+        }
+
+        if (iy < 0)
+        {
+            position.y = GameManager.Instance.Map.SizeY;
+            nextPosition.y = position.y - 0.5f;
+        }
+        else if (iy > GameManager.Instance.Map.SizeY)
+        {
+            position.y = 0; nextPosition.y = 0.5f;
+        }
+    }
+
+    public void OnDie()
+    {
+        _anim.SetBool("IsDead", true);
+        _direction = Direction.None;
+        _isDead = true;
+        Destroy(this.gameObject, 3);
+    }
+}
